eb662e9559d596c98526a4a2f0c465ee892cae74
AdvancedUno <ehlee8276@gmail.com>
2024-06-20 23:41:14 -0400

    Event pfocessing

---
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,45 @@
+using System.Text.Json;
+using AutoMapper;
+using CommandsService.Dtos;
+
+namespace CommandsService.EventProcessing
+{
+    public class EventProcessor : IEventProcessor
+    {
+        private readonly IServiceScopeFactory _scopeFactor;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(IServiceScopeFactory scopeFactor, IMapper mapper)
+        {
+            _scopeFactor = scopeFactor;
+            _mapper = mapper;
+            
+        }
+        public void ProcessEvent(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private EventType DetermineEvent(string notificationMessage){
+            Console.WriteLine("--> Determining Event");
+
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+            
+            switch(eventType.Event)
+            {
+                case "Uno_Published":
+                    Console.WriteLine("--> Uno Published Event Detected");
+                    return EventType.UnoPublished;
+                default:
+                    Console.WriteLine("--> Could Not Determine the Event Type");
+                    return EventType.Undetermined;
+            }
+        }
+    }
+
+    enum EventType
+    {
+        UnoPublished,
+        Undetermined
+    }
+}
